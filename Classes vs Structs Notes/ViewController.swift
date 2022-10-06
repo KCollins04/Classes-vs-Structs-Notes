55613f49c4298d67ef5adcb5604458c83473178c
ok <kelsey.collins23@d155.org>
2022-10-06 07:34:26 -0500

    Initial Commit

---
+++ Classes vs Structs Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Classes vs Structs Notes
+//
+//  Created by KELSEY COLLINS on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
